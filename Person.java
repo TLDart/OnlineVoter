16dd791ac2b06926b2001b8b19e7a71d7a9d78c6
Duarte Manuel Bento Dias <duartedias@student.dei.uc.pt>
2021-03-20 15:11:18 +0000

    Initial Commit

---
+++ Person.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+import java.sql.Date;
+
+import jdk.jfr.Unsigned;
+
+import java.util.Calendar;
+
+public class Person implements Serializable{
+   private long uid;
+   private String name;
+   private String password;
+   private String dep;
+   private String address;
+   private int phoneNumber;
+   private int ccNr;
+   private Calendar ccValidity;
+
+   Person(long uid, String name, String password, String dep, String address, int phoneNumber, int ccNr, Calendar ccValidity){
+       this.uid = uid;
+       this.name = name;
+       this.password = password;
+       this.dep = dep;
+       this.address = address;
+       this.phoneNumber = phoneNumber;
+       this.ccNr = ccNr;
+       this.ccValidity = ccValidity;
+   }
+}
